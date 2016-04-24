682a8d59e5ed871d9865b72f30d44e1f1b5dc49f
RandyBoy <75268967@qq.com>
2016-04-25 01:17:44 +0800

    add

--- client/app.ts
+++ client/app.ts
@@ -1,34 +1,48 @@
 import 'zone.js/dist/zone';
 import 'reflect-metadata';
-import {Component} from 'angular2/core';
+import {Component, provide} from 'angular2/core';
 // import {bootstrap} from 'angular2/platform/browser';
 import {bootstrap} from 'angular2-meteor-auto-bootstrap';
 import {Parties} from '../collections/parties';
 import {Mongo} from 'meteor/mongo';
+import { Meteor } from 'meteor/meteor';
 import {RequestService} from '../collections/requestService';
 import {Http, HTTP_PROVIDERS} from 'angular2/http';
 import {TodoItem} from "../collections/TodoItem";
+import {ROUTER_PROVIDERS, ROUTER_DIRECTIVES, RouteConfig, APP_BASE_HREF} from 'angular2/router';
+import {PartiesList} from './parties-list/parties-list';
+import {PartyDetails} from './party-details/party-details';
 
 @Component({
     selector: 'app',
     templateUrl: 'client/app.html',
+    directives: [ROUTER_DIRECTIVES],
     providers: [RequestService, HTTP_PROVIDERS]
 })
+@RouteConfig([
+    { path: '/', as: 'PartiesList', component: PartiesList },
+    { path: '/party/:partyId', as: 'PartyDetails', component: PartyDetails }
+])
+
 export class App {
-    parties: Mongo.Cursor<Object>;
+    parties: Mongo.Cursor<Party>;
     result: any;
     todoItems: TodoItem[];
     todoJson: string;
     errorMessage: any;
 
     constructor(private requestService: RequestService) {
-        this.parties = Parties.find();
+        //  this.parties = Parties.find();
+        Meteor.subscribe('parties', () => {
+            this.parties = Parties.find();
+        });
         requestService.GetTodoItems().then(res => {
             this.result = JSON.stringify(res.json());
         });
     }
 
     UpdateTodoItem() {
+
         this.requestService
             .AddTodoItem(35, "add todo item", true)
             .subscribe(data => {
@@ -39,4 +53,4 @@ export class App {
     }
 
 }
-bootstrap(App);
+bootstrap(App, [ROUTER_PROVIDERS, provide(APP_BASE_HREF, { useValue: '/' })]);
--- server/main.ts
+++ server/main.ts
@@ -3,6 +3,7 @@ import { Meteor } from 'meteor/meteor';
 //import { HTTP } from 'meteor/http';
 //import {Parties} from '../collections/parties';
 import {loadParties} from './load-parties';
+import './parties.ts';
 
 Meteor.startup(loadParties);
 
